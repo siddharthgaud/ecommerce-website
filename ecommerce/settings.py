b194e0b66e1a24d2a872e43aaa081fe9c6d76a12
Siddharth <gaudsiddharth456@gmail.com>
2024-12-31 01:02:35 +0530

    setting 3

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -137,11 +137,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATIC_URL = 'static/'
 
