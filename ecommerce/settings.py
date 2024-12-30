ac283fe3ef7f8674038896d2b180fbd3e37238ea
Siddharth <gaudsiddharth456@gmail.com>
2024-12-30 19:26:34 +0530

    settings updated 5

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 # MEDIA_URL = '/image/download/'
-MEDIA_URL = '/media/'
-MEDIA_URL = os.path.join(BASE_DIR,'media')
+# MEDIA_URL = '/media/'
+# MEDIA_URL = os.path.join(BASE_DIR,'media')
 # MEDIA_ROOT = BASE_DIR
 
 # # Default primary key field type
@@ -136,12 +136,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static'),
+# ]
+
+
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
