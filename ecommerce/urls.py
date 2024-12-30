1838c031ee168ffc28e2dc115d20c6b781536b78
Siddharth <gaudsiddharth456@gmail.com>
2024-12-30 23:32:00 +0530

    settings updated 3

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,4 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls')),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-+ static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+
